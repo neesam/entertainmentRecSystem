7df1b0fb49350a9d5f6847604f9123b576518f1a
Andy Nees <neesam@mymail.vcu.edu>
2024-12-12 01:42:28 -0700

    modal

--- my-react-app/src/Components/Card.jsx
+++ my-react-app/src/Components/Card.jsx
@@ -1,16 +1,35 @@
+import { useState } from 'react';
+
 import React from 'react';
 
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import Modal from 'react-bootstrap/Modal'
+import Form from 'react-bootstrap/Form'
 
 
-const EntCard = ({clickFunction, deleteFunction, attributes}) => {
+const EntCard = ({ clickFunction, deleteFunction, attributes, submitForm }) => {
+    const [showModal, setShowModal] = useState(false);
+    const [selectedOption, setSelectedOption] = useState(""); // State for dropdown selection
+
+    const handleModalOpen = () => setShowModal(true);
+    const handleModalClose = () => {
+        if (selectedOption) {
+            submitForm(selectedOption);
+        }
+        setShowModal(false);
+    }
+
+    const handleOptionChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
+
     return (
         <div style={{
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
-            height: '25vh',
+            height: '33.35vh',
             backgroundColor: attributes.color 
         }}>
             <Card style={{
@@ -21,7 +40,7 @@ const EntCard = ({clickFunction, deleteFunction, attributes}) => {
                 flexDirection: 'column',
                 position: 'relative' 
             }}>
-                <Button
+                {attributes.type !== 'show' && <Button
                     style={{
                         position: 'absolute',
                         top: '10px',
@@ -31,27 +50,65 @@ const EntCard = ({clickFunction, deleteFunction, attributes}) => {
                     variant="outline-secondary"
                     onClick={handleModalOpen}>
                     &#9998; 
-                </Button>
+                </Button>}
 
                 <Card.Body>
                     <Card.Title>{attributes.title}</Card.Title>
                 </Card.Body>
                 <div>
-                    <Button 
-                        style={{marginLeft: '5%', marginRight: '5%', marginBottom: '3%'}} 
+                    <Button
+                        style={{ marginLeft: '5%', marginRight: '5%', marginBottom: '3%' }}
                         onClick={clickFunction}>
                         Get {attributes.type}
                     </Button>
-                    <Button 
-                        style={{marginRight: '5%', marginBottom: '3%'}} 
+                    <Button
+                        style={{ marginRight: '5%', marginBottom: '3%' }}
                         variant='danger'
                         onClick={deleteFunction}>
                         Delete {attributes.type}
                     </Button>
                 </div>
             </Card>
+
+            <Modal show={showModal} onHide={handleModalClose}>
+                <Modal.Header closeButton>
+                    {attributes.type === 'album' ? (
+                        <Modal.Title>
+                        Get an {attributes.type} from a specific table
+                        </Modal.Title>
+                    ) : 
+                        <Modal.Title>
+                        Get a {attributes.type} from a specific table
+                        </Modal.Title>}
+                </Modal.Header>
+                <Modal.Body>
+                <Form>
+                        <Form.Group controlId="formDropdown">
+                            <Form.Label>Choose an Option</Form.Label>
+                            <Form.Control as="select" value={selectedOption} onChange={handleOptionChange}>
+                                <option value="">-- Select an option --</option>
+                                {attributes.tables.map((table, index) => (
+                                    <option key={index} value={table}>
+                                        {table}
+                                    </option>
+                                ))}
+                            </Form.Control>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleModalClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={() => {
+                        handleModalClose();
+                    }}>
+                        Get {attributes.type}
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
-    )
-}
+    );
+};
 
 export default EntCard;
